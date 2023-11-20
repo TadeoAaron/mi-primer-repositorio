09bf84f50a53fab57642776ae499effcfe2e3fda
TadeoAaron <tadeoaaronceballosz@gmail.com>
2023-11-20 15:49:59 -0600

    modificacion7

--- codigo.cpp
+++ codigo.cpp
@@ -1,5 +1,6 @@
 codigo
 #include <iostream>
+#include <limits> // Para limpiar el buffer del cin después de leer un tipo incorrecto
 
 using namespace std;
 
@@ -7,6 +8,7 @@ int main()
 {
     int vida = 1;  // Elemento recolectable: vida
     int decision;
+    int decisionJuego;  // Nueva variable para manejar las elecciones dentro del bucle interno
 
     cout << "****** ¡Bienvenido a la historia interactiva! ******" << endl;
 
@@ -17,6 +19,11 @@ int main()
         cout << "1. Iniciar juego" << endl;
         cout << "2. Salir" << endl;
         cout << "Selecciona una opción: ";
+
+        // Limpiar el buffer del cin antes de leer la opción
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
         cin >> decision;
 
         switch (decision)
@@ -32,14 +39,21 @@ int main()
                     cout << "3. Agarrar la pala que está tirada" << endl;
                     cout << "4. Ponerte a pensar, ¿por qué estás aquí?" << endl;
                     cout << "-- Estado actual --" << endl;
-                    cout << "      Vida:  " << vida << endl;
-                    cout << "5. Regresar al menu principal" << endl;
+                    cout << "Vida: " << vida << endl;
+                    cout << "5. Regresar al Menú Principal" << endl;
                     cout << "Selecciona una opción: ";
-                    cin >> decision;
 
-                    switch (decision)
+                    // Limpiar el buffer del cin antes de leer la opción
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+                    cin >> decisionJuego;  // Utilizamos una nueva variable
+
+                    switch (decisionJuego)
                     {
+                        // Código de acciones
                         case 1:
+                            // Código para la opción 1
                             cout << "--- Estado actual ---" << endl;
                             cout << "     Vida: " << vida << endl;
                             cout << "-- Te aventuras en el bosque. ¿Qué decides hacer? --" << endl;
@@ -47,14 +61,14 @@ int main()
                             cout << "2. Seguir a la derecha." << endl;
                             cout << "3. Seguir la luz misteriosa." << endl;
                             cout << "Selecciona una opción: ";
-                            cin >> decision;
+                            cin >> decisionJuego;
 
-                            if (decision == 1)
+                            if (decisionJuego == 1)
                             {
                                 cout << "Encuentras un hombre misterioso de espaldas, Te golpean en la cabeza." << endl;
                                 vida--;
                             }
-                            else if (decision == 2)
+                            else if (decisionJuego == 2)
                             {
                                 cout << "Sigues caminando un rato pero no ves un hoyo, tropiezas y te golpeas la cabeza." << endl;
                                 vida--;
@@ -65,7 +79,9 @@ int main()
                                 vida++;
                             }
                             break;
+                            
                         case 2:
+                            // Código para la opción 2
                             cout << "--- Estado actual ---" << endl;
                             cout << "Vida: " << vida << endl;
                             cout << "-- Decidiste mirar a la luna. ¿Sera buena idea? --" << endl;
@@ -73,13 +89,13 @@ int main()
                             cout << "2. Dejar de mirar " << endl;
                             cout << "3. HUIR" << endl;
                             cout << "Selecciona una opción: ";
-                            cin >> decision;
+                            cin >> decisionJuego;
 
-                            if (decision == 1) {
+                            if (decisionJuego == 1) {
                                 cout << "Esa no es la luna" << endl;
                                 vida--;
                             } 
-                            else if (decision == 2)
+                            else if (decisionJuego == 2)
                             {
                                 cout << "Buena elección, esa cosa no era la luna." << endl;
                                 vida++;
@@ -91,19 +107,20 @@ int main()
                             }
                             break;
                         case 3:
+                            // Código para la opción 3
                             cout << "\nEncuentras a un aldeano herido. ¿Cómo decides ayudar?\n";
                             cout << "1. Utilizar una poción para curarlo\n";
                             cout << "2. Buscar ayuda en la aldea\n";
                             cout << "3. Ignorar y seguir explorando\n";
                             cout << "Selecciona una opción: ";
-                            cin >> decision;
+                            cin >> decisionJuego;
 
-                            if (decision == 1)
+                            if (decisionJuego == 1)
                             {
                                 cout << "Utilizas una poción para curar al aldeano. Ganas su gratitud y 1 punto de vida.\n";
                                 vida++;
                             } 
-                            else if (decision == 2) 
+                            else if (decisionJuego == 2) 
                             {
                                 cout << "Buscas ayuda en la aldea y recibes 2 puntos de vida como agradecimiento.\n";
                                 vida += 2;
@@ -114,19 +131,20 @@ int main()
                             }
                             break;
                         case 4:
+                            // Código para la opción 4
                             cout << "\nDecides descansar en la posada. ¿Qué actividad eliges?\n";
                             cout << "1. Comprar pociones\n";
                             cout << "2. Participar en un torneo de habilidades\n";
                             cout << "3. Descansar y recuperar energías\n";
                             cout << "Selecciona una opción: ";
-                            cin >> decision;
+                            cin >> decisionJuego;
 
-                            if (decision == 1) 
+                            if (decisionJuego == 1) 
                             {
                                 cout << "Compras pociones adicionales. Gastas 2 puntos de vida.\n";
                                 vida -= 2;
                             } 
-                            else if (decision == 2)
+                            else if (decisionJuego == 2)
                             {
                                 cout << "Participas en un torneo y ganas un premio. Recibes 3 puntos de vida.\n";
                                 vida += 3;
@@ -142,7 +160,7 @@ int main()
                             break;
                         default:
                             cout << "Opción no válida. Inténtalo de nuevo." << endl;
-                            break;
+                             break;
                     }
 
                     if (vida <= 0) 
